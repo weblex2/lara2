1ac1cb12d3213672919aaa9d64a72a3c3006f96b
weblex2 <noppenbe@gmx.de>
2021-08-14 19:05:08 +0200

    vendor 2

--- vendor2/tightenco/ziggy/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: ['@babel/plugin-proposal-optional-chaining'],
-    env: {
-        test: {
-            presets: ['power-assert'],
-            plugins: ['@babel/plugin-transform-modules-commonjs'],
-        },
-    },
-};
