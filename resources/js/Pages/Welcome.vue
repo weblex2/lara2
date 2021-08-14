7fb5df4a418ab6b2a1bb3a647469ebcbb28ce748
weblex2 <noppenbe@gmx.de>
2021-08-14 20:00:18 +0200

    Test

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -37,7 +37,7 @@
 
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                111 Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                             </div>
                         </div>
                     </div>
@@ -50,7 +50,7 @@
 
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                1111 Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
                             </div>
                         </div>
                     </div>
