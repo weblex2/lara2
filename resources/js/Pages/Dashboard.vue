77dd612b4566476ef5ee7d1e9b09bb38fdfc462c
weblex2 <noppenbe@gmx.de>
2021-08-15 17:31:45 +0200

    test

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,8 +2,8 @@
     <app-layout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
+                Dashboard 1
+             </h2>
         </template>
 
         <div class="py-12">
